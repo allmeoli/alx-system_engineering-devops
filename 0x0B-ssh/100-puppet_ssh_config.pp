7c00b397491c53f5b098c5f83ab439e674bc3283
callmeoli <olikorma@gmail.com>
2022-12-10 10:37:34 +0300

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # puppet manifest that make client config
 
-file { '/etc/ssh/ssh_config':
+file_line { '/etc/ssh/ssh_config':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
 }
 
-file { '/etc/ssh/ssh_config':
+file_line { '/etc/ssh/ssh_config':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/school',
