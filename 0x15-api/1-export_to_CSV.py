88286ff51f9fd3b8fa089c14ac142a73283a51a8
callmeoli <olikorma@gmail.com>
2023-01-17 04:06:02 +0300

    export_to_csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Script that uses JSONPlaceholder API to get information about employee """
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+
+    user = '{}users/{}'.format(url, sys.argv[1])
+    res = requests.get(user)
+    json_o = res.json()
+    name = json_o.get('name')
+
+    id = sys.argv[1]
+    todos = '{}todos?userId={}'.format(url, sys.argv[1])
+    res = requests.get(todos)
+    tasks = res.json()
+    l_task = []
+
+    name_first = name.split(" ")[0]
+    with open('{}.csv'.format(id), 'w', encoding='UTF8') as f:
+        for task in tasks:
+            writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+            lst = [
+                str(id), name_first, str(
+                    task.get('completed')), str(
+                    task.get('title'))]
+            writer.writerow(lst)
